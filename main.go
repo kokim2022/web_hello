a57535ece0454fcde21174b878c4bf665de11932
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-06-06 17:25:18 +0700

    numberic types and converstion added

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/kokim2020/web_hello/number_types"
 )
 
 /*
 All Go programs start running from a function called `main` in a package called `main`
 */
 func main() {
+	number_types.PrintHello()
 	http.HandleFunc("/hello", func(rw http.ResponseWriter, req *http.Request) {
 		name := req.URL.Query().Get("name")
 		rw.Write([]byte(fmt.Sprintf("Hello, %s", name)))
---
+++ number_types/number_types.go
@@ -0,0 +1,11 @@
+package number_types
+
+import "fmt"
+
+func PrintHello() {
+	var i int = 590
+	var f int32 = 545
+	// float32 is used to covert integer into the float32
+	// fmt.Println(float32(i) + f)
+	fmt.Println(int32(i) + f)
+}
