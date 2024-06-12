09a4c44e297d35e5c0a3493676af4e918f368697
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-06-13 00:00:46 +0700

    rune and array added

--- learn_strings/learn_strings.go
+++ learn_strings/learn_strings.go
@@ -3,5 +3,24 @@ package learn_strings
 import "fmt"
 
 func LearnStrings() {
-	fmt.Println("testing")
+	// assign string in the variable
+	s := `Hello,"DEMO" 
+						how are u?`
+	s2 := s[2:4]
+	// assign array into the numbers variable
+	numbers := [...]int{1, 2, 3, 4, 5}
+	// You can use runes to work with Unicode characters.
+	// For example, you can iterate over the characters in a string,
+	// which might contain multi-byte UTF-8 encoded characters,
+	// and work with each character as a rune
+	r3 := rune(0x1F600)
+	fmt.Println(numbers)
+	fmt.Println(s2)
+	// print unicode rune character
+	fmt.Println(string(r3))
+	// Rune Literal
+	// A rune literal is just a character enclosed in single quotes.
+	// You can also use Unicode escape sequences to represent runes:
+	r4 := '\u4e16'
+	fmt.Println(string(r4))
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/kokim2020/web_hello/learn_strings"
 	"github.com/kokim2020/web_hello/number_types"
 )
 
@@ -9,6 +10,7 @@ All Go programs start running from a function called `main` in a package called 
 */
 func main() {
 	number_types.PrintInteger()
+	learn_strings.LearnStrings()
 	// http.HandleFunc("/hello", func(rw http.ResponseWriter, req *http.Request) {
 	// 	name := req.URL.Query().Get("name")
 	// 	rw.Write([]byte(fmt.Sprintf("Hello, %s", name)))
