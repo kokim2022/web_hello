e15781923aa21993d57eced72a4b82f031ae4852
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-06-13 00:36:49 +0700

    learn if and for initialized

---
+++ learn_if_for/learn_if_for.go
@@ -0,0 +1,7 @@
+package learn_if_for
+
+import "fmt"
+
+func LearnIfFor() {
+	fmt.Printf("learn if and for")
+}
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"github.com/kokim2020/web_hello/learn_strings"
-	"github.com/kokim2020/web_hello/number_types"
+	"github.com/kokim2020/web_hello/learn_if_for"
 )
 
 /*
 All Go programs start running from a function called `main` in a package called `main`
 */
 func main() {
-	number_types.PrintInteger()
-	learn_strings.LearnStrings()
+	// number_types.PrintInteger()
+	// learn_strings.LearnStrings()
+	learn_if_for.LearnIfFor()
 	// http.HandleFunc("/hello", func(rw http.ResponseWriter, req *http.Request) {
 	// 	name := req.URL.Query().Get("name")
 	// 	rw.Write([]byte(fmt.Sprintf("Hello, %s", name)))
