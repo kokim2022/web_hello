2504fed160246f8d004fdc5b4b988ce923e73185
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-06-13 00:41:55 +0700

    condition statement added

--- learn_if_for/learn_if_for.go
+++ learn_if_for/learn_if_for.go
@@ -3,5 +3,10 @@ package learn_if_for
 import "fmt"
 
 func LearnIfFor() {
-	fmt.Printf("learn if and for")
+	a := 5
+	if a > 5 {
+		fmt.Println("a is greater than ")
+	} else {
+		fmt.Println("a is less than or equal to 5")
+	}
 }
