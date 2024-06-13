822637fe142102c91f28c170e44ee9edf69ada6c
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-06-13 10:29:56 +0700

    for loop testing ok

--- learn_if_for/learn_if_for.go
+++ learn_if_for/learn_if_for.go
@@ -2,12 +2,18 @@ package learn_if_for
 
 import "fmt"
 
-func LearnIfFor() {
+func LearnIf() {
 	a := 10
 	// initialization within the if statement
 	if b := a / 2; b > 5 {
 		fmt.Println("a is greater than ")
 	} else {
 		fmt.Println("a is less than or equal to 5:", a, b)
+	}
+}
+
+func LearnFor() {
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
 	}
 }
