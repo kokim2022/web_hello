bfea8924977813a75857067223b8ae528da6cecb
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-07-04 01:24:30 +0700

    learn function .go

--- learn_funcs/learn_funcs.go
+++ learn_funcs/learn_funcs.go
@@ -1,7 +1,9 @@
 package learn_funcs
 
-import "fmt"
+func AddNumbers(a int, b int) int {
+	return a + b
+}
 
-func AddNumbers(a int, b int) {
-	fmt.Println(a + b)
+func DivAndRemainder(a int, b int) (int, int) {
+	return a / b, a % b
 }
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/kokim2020/web_hello/learn_funcs"
-
 /*
 All Go programs start running from a function called `main` in a package called `main`
 */
@@ -9,13 +7,27 @@ func main() {
 	// number_types.PrintInteger()
 	// learn_strings.LearnStrings()
 	// learn_switch.LearnSwitchWithBoolean()
-	learn_funcs.AddNumbers(1, 2)
-	learn_funcs.AddNumbers(3, 4)
-	learn_funcs.AddNumbers(3, 5)
-
+	// a := learn_funcs.AddNumbers(1, 2)
+	// fmt.Println(a)
+	// a = learn_funcs.AddNumbers(3, 4)
+	// fmt.Println(a)
+	// a = learn_funcs.AddNumbers(3, 5)
+	// fmt.Println(a)
 	// http.HandleFunc("/hello", func(rw http.ResponseWriter, req *http.Request) {
 	// 	name := req.URL.Query().Get("name")
 	// 	rw.Write([]byte(fmt.Sprintf("Hello, %s", name)))
 	// })
 	// http.ListenAndServe(":8080", nil)
+
+	// When a function returns multiple values,
+	//  but you are interested in only some of them,
+	// you can use _ to ignore the values you don't need.
+	// div, remainder := learn_funcs.DivAndRemainder(2, 3)
+	// fmt.Println(div, remainder)
+
+	// div, _ = learn_funcs.DivAndRemainder(10, 2)
+	// fmt.Println(div)
+
+	// _, remainder = learn_funcs.DivAndRemainder(100, -100)
+	// fmt.Println(remainder)
 }
